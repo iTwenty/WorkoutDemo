2bf28f19284e20d5be2b23ed371ae72180115ace
jaydeep-helpshift <jaydeep@helpshift.com>
2024-12-18 11:15:27 +0530

    Initial Commit

---
+++ WorkoutDemo/WorkoutDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutDemoApp.swift
+//  WorkoutDemo
+//
+//  Created by Jaydeep Joshi on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
